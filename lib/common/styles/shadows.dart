f22a0ce5d720351f6c993448e39900eda80fae7f
siraajul <shirajulislamparvez@gmail.com>
2023-12-28 18:02:40 +0600

    Custom Container Shadow

---
+++ lib/common/styles/shadows.dart
@@ -0,0 +1,16 @@
+import 'package:fire_commerce/utils/constants/colors.dart';
+import 'package:flutter/cupertino.dart';
+
+class TShadowStyle {
+  static final verticalProductShadow = BoxShadow(
+      color: TColors.darkGrey.withOpacity(0.1),
+      blurRadius: 50,
+      spreadRadius: 7,
+      offset: const Offset(0, 2));
+
+  static final horizontalProductShadow = BoxShadow(
+      color: TColors.darkGrey.withOpacity(0.1),
+      blurRadius: 50,
+      spreadRadius: 7,
+      offset: const Offset(0, 2));
+}
