26d49bcaf674b7f5564f43390f65f3fd49857730
siraajul <shirajulislamparvez@gmail.com>
2024-01-01 23:27:42 +0600

    Test Screen for Check Custom Widgets

--- lib/test_screen.dart
+++ lib/test_screen.dart
@@ -1,4 +1,6 @@
 import 'package:fire_commerce/utils/constants/colors.dart';
+import 'package:fire_commerce/utils/constants/image_strings.dart';
+import 'package:fire_commerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 import 'common/products/product_cards/product_card_vertical.dart';
@@ -8,13 +10,18 @@ class TestScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
+    return   Scaffold(
       backgroundColor: TColors.light,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ProductCardVertical()
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(TSizes.md),
+              ),
+              child: Image(image: AssetImage(TImages.promoBanner1),),
+            )
           ],
         ),
       ),
